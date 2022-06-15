66b84534e18a66d6bd07a0f038d9880c2bfe3d97
root <root@pinet-wedge100-2-eth0.cs.lan>
2022-06-15 17:14:03 -0300

    config for gpu6

--- run_bfrt_python/setup.py
+++ run_bfrt_python/setup.py
@@ -1,21 +1,21 @@
-table1 = bfrt.netgvt_l2_16.pipe.SwitchIngress.ipv4_lpm
+table1 = bfrt.NetGVTapp.pipe.SwitchIngress.ipv4_lpm
 entry = table1.entry_with_ipv4_forward(dst_addr=0x0a320106 , dst_mac=0xb8599fdf07cb, port="188").push()
 
 entry = table1.entry_with_ipv4_forward(dst_addr=0x0a320101 , dst_mac=0x00154d1211a9, port="132").push()
 
-table2 = bfrt.netgvt_l2_16.pipe.SwitchIngress.eth_forward
+table2 = bfrt.NetGVTapp.pipe.SwitchIngress.eth_forward
 entry = table2.entry_with_gvt_forward(src_addr=0x00154d1211a9 , dst_mac=0x00154d1211a9, port="132").push()
 
-bfrt.netgvt_l2_16.pipe.LVT_pid_0.dump(from_hw=True)
-bfrt.netgvt_l2_16.pipe.LVT_pid_1.dump(from_hw=True)
-bfrt.netgvt_l2_16.pipe.GVT.dump(from_hw=True)
+bfrt.NetGVTapp.pipe.LVT_pid_0.dump(from_hw=True)
+bfrt.NetGVTapp.pipe.LVT_pid_1.dump(from_hw=True)
+bfrt.NetGVTapp.pipe.GVT.dump(from_hw=True)
 
-bfrt.netgvt_l2_16.pipe.LVT_pid_1.add(0, 100000)
-bfrt.netgvt_l2_16.pipe.LVT_pid_2.add(0, 100000)
-bfrt.netgvt_l2_16.pipe.LVT_pid_3.add(0, 100000)
-bfrt.netgvt_l2_16.pipe.LVT_pid_4.add(0, 100000)
-bfrt.netgvt_l2_16.pipe.LVT_pid_5.add(0, 100000)
-bfrt.netgvt_l2_16.pipe.LVT_pid_6.add(0, 100000)
+bfrt.NetGVTapp.pipe.LVT_pid_1.add(0, 100000)
+bfrt.NetGVTapp.pipe.LVT_pid_2.add(0, 100000)
+bfrt.NetGVTapp.pipe.LVT_pid_3.add(0, 100000)
+bfrt.NetGVTapp.pipe.LVT_pid_4.add(0, 100000)
+bfrt.NetGVTapp.pipe.LVT_pid_5.add(0, 100000)
+bfrt.NetGVTapp.pipe.LVT_pid_6.add(0, 100000)
 
 
 #Setup multicast group
