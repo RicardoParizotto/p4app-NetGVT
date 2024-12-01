430bf1b1fe938cf78d79ea73a0c1d9e682668707
Ricardo Parizotto <ricardo.dparizotto@gmail.com>
2024-12-01 11:29:20 -0300

    server print packet

--- server/client.cpp
+++ server/client.cpp
@@ -85,7 +85,7 @@ void send_packets(const char *src_ip, int end_simulation_loop) {
 
     struct ether_header eth_hdr;
     memset(&eth_hdr, 0, sizeof(eth_hdr));
-    eth_hdr.ether_type = htons(ETHERTYPE_GVT);
+    eth_hdr.ether_type = ETHERTYPE_GVT;
     memset(eth_hdr.ether_dhost, 0xFF, sizeof(eth_hdr.ether_dhost));
 
     uint8_t manual_mac[6] = {0x94, 0x6d, 0xae, 0x5c, 0x87, 0x72};
@@ -98,7 +98,7 @@ void send_packets(const char *src_ip, int end_simulation_loop) {
         if (lvt <= gvt) {
             lvt++;
 
-            GvtProtocol gvt_hdr = {TYPE_PROPOSAL, htonl(lvt), htonl(pid), htonl(gvt)};
+            GvtProtocol gvt_hdr = {TYPE_PROPOSAL, lvt, pid, gvt};
 
             uint8_t packet[sizeof(ether_header) + sizeof(GvtProtocol)];
             memcpy(packet, &eth_hdr, sizeof(ether_header));
