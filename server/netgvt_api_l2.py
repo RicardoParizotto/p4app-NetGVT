6cccdb23f961fc7029a4c5c99a67b7be28085f49
ricardoparizotto <ricardo.dparizotto@gmail.com>
2024-12-08 10:01:47 -0300

    server results

--- server/netgvt_api_l2.py
+++ server/netgvt_api_l2.py
@@ -105,6 +105,9 @@ def send(iface, end_time):
     total = str(end-start)
     print("total time: " + total)
 
+    current_file_path = os.path.abspath(__file__)
+    print(current_file_path)
+
     file = open(f"results/{mode}_pid{str(pid)}.txt", "a+")
     file.write("total time, " + total + "\n") 
     file.close()
