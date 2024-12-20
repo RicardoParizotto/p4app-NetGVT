f1abadbccf9a59e83ae46129589c74a335d94c9f
ricardoparizotto <ricardo.dparizotto@gmail.com>
2024-12-20 19:08:44 +0000

    iperf experiments with different recirculations

--- throughput/run_iperf.sh
+++ throughput/run_iperf.sh
@@ -4,13 +4,13 @@
 SERVER_IP="10.0.1.2"
 
 # Define the output file for the results
-OUTPUT_FILE="iperf_results_with_64.txt"
+OUTPUT_FILE="iperf_results_with_16.txt"
 
 # Initialize the output file
-echo "Running iperf test 50 times..." > $OUTPUT_FILE
+echo "Running iperf test 100 times..." > $OUTPUT_FILE
 
 # Run the test 50 times
-for i in {1..50}
+for i in {1..100}
 do
     echo "Running iperf test $i..." >> $OUTPUT_FILE
     # Run the iperf client command (adjust parameters as needed)
